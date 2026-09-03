ef7c4810fc801dc16364ad0e0106e49f21a789c3
blandow <felipeblandowsiqueira@gmail.com>
2026-09-03 16:47:47 -0300

    FEAT: Aluno CreateDTO

--- TesteEscolaAPI/DTOs/Requests/AlunoCreateDto.cs
+++ TesteEscolaAPI/DTOs/Requests/AlunoCreateDto.cs
@@ -7,6 +7,8 @@ namespace TesteEscolaAPI.DTOs.Requests
 {
     public class AlunoCreateDto
     {
-        
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
     }
 }
