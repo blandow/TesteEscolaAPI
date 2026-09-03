78946e52bb0888cbf97ff0f76ec7fa6d4e8879ca
blandow <felipeblandowsiqueira@gmail.com>
2026-09-03 16:51:55 -0300

    FEAT: Aluno updateDTO

--- TesteEscolaAPI/DTOs/Requests/AlunoUpdateDto.cs
+++ TesteEscolaAPI/DTOs/Requests/AlunoUpdateDto.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+
 
 namespace TesteEscolaAPI.DTOs.Requests
 {
     public class AlunoUpdateDto
     {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public DateTime DataNascimento { get; set; }
     }
 }
