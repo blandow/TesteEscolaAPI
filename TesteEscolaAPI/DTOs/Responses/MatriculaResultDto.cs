cdaa389a5586bad41842679ecb351bde6992b739
blandow <felipeblandowsiqueira@gmail.com>
2026-09-03 12:38:40 -0300

    FEAT: Initial project structure

---
+++ TesteEscolaAPI/DTOs/Responses/MatriculaResultDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TesteEscolaAPI.DTOs.Responses
+{
+    public class MatriculaResultDto
+    {
+    }
+}
