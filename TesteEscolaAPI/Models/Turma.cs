c9e26e84c4ba1b55eac84e2aa023dfc42741bde0
blandow <felipeblandowsiqueira@gmail.com>
2026-09-04 09:46:42 -0300

    FEAT: implement Turma Model

--- TesteEscolaAPI/Models/Turma.cs
+++ TesteEscolaAPI/Models/Turma.cs
@@ -7,5 +7,11 @@ namespace TesteEscolaAPI.Models
 {
     public class Turma
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Periodo { get; set; }
+        public int VagasTotal { get; set; }
+        public int VagasDisponiveis { get; set; }
+
     }
 }
