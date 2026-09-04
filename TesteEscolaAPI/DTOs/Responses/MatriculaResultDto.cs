6dd2a1659d403ee2e1049e51218d0d470a787e70
blandow <felipeblandowsiqueira@gmail.com>
2026-09-04 09:58:44 -0300

    FEAT: Implement Matricula Result DTO

--- TesteEscolaAPI/DTOs/Responses/MatriculaResultDto.cs
+++ TesteEscolaAPI/DTOs/Responses/MatriculaResultDto.cs
@@ -7,5 +7,10 @@ namespace TesteEscolaAPI.DTOs.Responses
 {
     public class MatriculaResultDto
     {
+        public int Id { get; set; }
+        public int AlunoId { get; set; }
+        public int TurmaId { get; set; }
+        public DateTime DataMatricula { get; set; }
+        public int VagasRestantes { get; set; }
     }
 }
